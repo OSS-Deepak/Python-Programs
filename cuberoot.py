1015e5d7592b373af2f7d5b6a94d3110eb6f3125
Deepak Garg <deepak.garg1@3pillarglobal.com>
2016-10-28 14:26:17 +0530

    cuberoot.py

--- cuberoot.py
+++ cuberoot.py
@@ -1,1 +1,25 @@
-print(deepak)
+import math
+def cubeRoot(n):
+	start = 0
+	end = n
+	e = 0.0000000000001
+	mid = 0
+	diff = 0
+	while True:
+		mid = (start + end) / 2
+		temp = mid*mid*mid
+		if n > temp:
+			diff = n - temp
+		else:
+			diff = temp - n
+
+		if diff <= e:
+			return mid 
+
+		if temp > n:
+			end = mid
+		else:
+			start = mid
+
+number = int(input())
+print(cubeRoot(number))
